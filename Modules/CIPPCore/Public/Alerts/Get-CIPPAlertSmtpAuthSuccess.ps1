73971a2c0ef1c5334c90be13fb4977717f6a1ab5
jtarran <james@tarrantech.co.uk>
2026-02-03 09:32:57 +0000

    more more more fix?

--- Modules/CIPPCore/Public/Alerts/Get-CIPPAlertSmtpAuthSuccess.ps1
+++ Modules/CIPPCore/Public/Alerts/Get-CIPPAlertSmtpAuthSuccess.ps1
@@ -1,42 +1,47 @@
 function Get-CIPPAlertSmtpAuthSuccess {
     [CmdletBinding()]
     param (
+        # REQUIRED: absorbs the ScheduledCommand object
+        [Parameter(ValueFromPipeline = $true)]
+        [object]$ScheduledCommand,
+
         [Parameter(Mandatory = $true)]
         $TenantFilter
     )
 
-    try {
-        $uri = "https://graph.microsoft.com/v1.0/auditLogs/signIns?`$filter=clientAppUsed eq 'Authenticated SMTP' and status/errorCode eq 0"
+    begin {
+        $Results = @()
+    }
 
+    process {
+        # Intentionally empty
+        # This stops the orchestrator object from flowing further
+    }
+
+    end {
+        $uri = "https://graph.microsoft.com/v1.0/auditLogs/signIns?`$filter=clientAppUsed eq 'Authenticated SMTP' and status/errorCode eq 0"
         $SignIns = New-GraphGetRequest -Uri $uri -TenantId $TenantFilter
 
-        if (-not $SignIns.value) {
-            return @()
+        if ($SignIns.value) {
+            $Results = @(
+                $SignIns.value | ForEach-Object {
+                    [pscustomobject]@{
+                        Tenant            = $TenantFilter
+                        UserPrincipalName = $_.userPrincipalName
+                        CreatedDateTime   = $_.createdDateTime
+                        ClientAppUsed     = $_.clientAppUsed
+                        IPAddress         = $_.ipAddress
+                        ErrorCode         = $_.status.errorCode
+                    }
+                }
+            )
+
+            Write-AlertTrace `
+                -cmdletName $MyInvocation.MyCommand `
+                -tenantFilter $TenantFilter `
+                -data $Results
         }
 
-        $AlertData = @(
-            $SignIns.value | ForEach-Object {
-                [pscustomobject]@{
-                    Tenant            = $TenantFilter
-                    UserPrincipalName = $_.userPrincipalName
-                    CreatedDateTime   = $_.createdDateTime
-                    ClientAppUsed     = $_.clientAppUsed
-                    IPAddress         = $_.ipAddress
-                    ErrorCode         = $_.status.errorCode
-                }
-            }
-        )
-
-        # Log for dedupe / history
-        Write-AlertTrace `
-            -cmdletName $MyInvocation.MyCommand `
-            -tenantFilter $TenantFilter `
-            -data $AlertData
-
-        # 🔑 THIS IS THE CRITICAL LINE
-        return $AlertData
-    }
-    catch {
-        return @()
+        return $Results
     }
 }
