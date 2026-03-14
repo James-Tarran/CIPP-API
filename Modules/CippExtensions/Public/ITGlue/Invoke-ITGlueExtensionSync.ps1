a6feb77a1d2748f766ce484e7961dd89113469b5
James Tarran <109956898+James-Tarran@users.noreply.github.com>
2026-03-14 01:05:07 +0000

    Update Invoke-ITGlueExtensionSync.ps1

--- Modules/CippExtensions/Public/ITGlue/Invoke-ITGlueExtensionSync.ps1
+++ Modules/CippExtensions/Public/ITGlue/Invoke-ITGlueExtensionSync.ps1
@@ -74,50 +74,21 @@ function Invoke-ITGlueExtensionSync {
                     $CAPTypeId = $ExistingCAPType.id
                     $CompanyResult.Logs.Add("Found existing Conditional Access flexible asset type: $($ExistingCAPType.name)")
                 } else {
-                    # Create new flexible asset type with all required fields
+                    # Create minimal flexible asset type first
                     $NewTypeBody = @{
                         data = @{
                             type       = 'flexible-asset-types'
                             attributes = @{
-                                name        = 'Conditional Access Policy'
-                                description = 'Microsoft 365 Conditional Access Policies synced from CIPP'
-                                icon        = 'shield-alt'
-                                enabled     = $true
+                                name                    = 'Conditional Access Policy'
+                                description             = 'Microsoft 365 Conditional Access Policies synced from CIPP'
+                                icon                    = 'shield-alt'
+                                enabled                 = $true
                                 'flexible-asset-fields' = @(
                                     @{
                                         name           = 'Policy Name'
                                         kind           = 'Text'
                                         required       = $true
                                         'show-in-list' = $true
-                                        position       = 1
-                                    }
-                                    @{
-                                        name           = 'Policy ID'
-                                        kind           = 'Text'
-                                        required       = $false
-                                        'show-in-list' = $false
-                                        position       = 2
-                                    }
-                                    @{
-                                        name           = 'State'
-                                        kind           = 'Text'
-                                        required       = $false
-                                        'show-in-list' = $true
-                                        position       = 3
-                                    }
-                                    @{
-                                        name           = 'Policy Details'
-                                        kind           = 'Textbox'
-                                        required       = $false
-                                        'show-in-list' = $false
-                                        position       = 4
-                                    }
-                                    @{
-                                        name           = 'Raw JSON'
-                                        kind           = 'Textbox'
-                                        required       = $false
-                                        'show-in-list' = $false
-                                        position       = 5
                                     }
                                 )
                             }
@@ -127,6 +98,12 @@ function Invoke-ITGlueExtensionSync {
                     $NewType = Invoke-RestMethod -Uri "$($Conn.BaseUrl)/flexible_asset_types" -Method POST -Headers $Conn.Headers -Body $NewTypeBody
                     $CAPTypeId = $NewType.data.id
                     $CompanyResult.Logs.Add("Created new Conditional Access Policy flexible asset type (ID: $CAPTypeId)")
+
+                    # Add remaining fields using the helper function
+                    Add-ITGlueFlexibleAssetField -TypeId $CAPTypeId -FieldName 'Policy ID' -FieldKind 'Text' -ShowInList $false -Conn $Conn
+                    Add-ITGlueFlexibleAssetField -TypeId $CAPTypeId -FieldName 'State' -FieldKind 'Text' -ShowInList $true -Conn $Conn
+                    Add-ITGlueFlexibleAssetField -TypeId $CAPTypeId -FieldName 'Policy Details' -FieldKind 'Textbox' -ShowInList $false -Conn $Conn
+                    Add-ITGlueFlexibleAssetField -TypeId $CAPTypeId -FieldName 'Raw JSON' -FieldKind 'Textbox' -ShowInList $false -Conn $Conn
                 }
 
                 # Save mapping to database
